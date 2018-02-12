601945168987d4ddcdd4bdc72c99e3b3aed43eed
Benjamin J Phillips <bephillips2@gmail.com>
2018-02-12 01:03:19 -0700

    Removed cached files

--- data/transfer_map.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-selection="$1"
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-gnuplot -e "filename='$selection'; outputname='$DIR/current_mode_map.jpeg'" $DIR/mode_map_plotter.plot
-scp $DIR/current_mode_map.jpeg kyou@kitsune.dyndns-ip.com:/mnt/data/www/html/Electric_Tiger/Current_Data/
--- data/transfer_power_spec.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-file_path="$1"
-plot_name="$2"
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-gnuplot -e "filename='$file_path'; outputname='$plot_name'" $DIR/power_spectrum_plotter.plot
-scp $plot_name kyou@kitsune.dyndns-ip.com:/mnt/data/www/html/Electric_Tiger/Current_Data/
